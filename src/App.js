b9239bd5039beeaf298eb9af0fdf53dabbf415d0
Rafif Agdyaputra <rafif.adgyaputra@int.youtap.id>
2023-06-15 17:15:48 +0700

    feat(master): add list credentials

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Home from "./Pages/Home/Home";
 import NotFound from "./Pages/NotFound";
 import Detail from "./Pages/Detail/Detail";
 import List from "./Pages/List/List";
+import {Box} from "@chakra-ui/react";
 
 function App() {
   return (
-    <Routes>
-      <Route path="*" element={<NotFound/>} />
-      <Route path="/" element={<Home/>} />
-      <Route path="/credentials" element={<List/>} />
-      <Route path="/credentials/detail/:id" element={<Detail/>} />
-    </Routes>
+    <Box padding='2'>
+      <Routes>
+        <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/credentials" element={<List/>} />
+        <Route path="/credentials/detail/:id" element={<Detail/>} />
+      </Routes>
+    </Box>
   );
 }
 
--- src/Pages/List/List.js
+++ src/Pages/List/List.js
@@ -1,7 +1,73 @@
+import {useCallback, useEffect, useState} from "react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Flex,
+  Heading,
+  Spacer,
+  Stack,
+  Text
+} from "@chakra-ui/react";
 
 const List = () => {
+  const [listCredentials, setListCredentials] = useState([]);
+
+  const GetListCredentials = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:3003/credentials')
+      const lists = await response.json();
+      setListCredentials(lists);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    GetListCredentials().then();
+  }, []);
+
   return (
-    <h1>List Component</h1>
+    <>
+      <Flex minWidth='max-content' alignItems='center' gap='2' marginBottom='4'>
+        <Box p='2'>
+          <Heading size='md'>Password Manager</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap='2'>
+          <Button colorScheme='teal'>Add</Button>
+        </ButtonGroup>
+      </Flex>
+      <Flex gap='3'>
+        {listCredentials && listCredentials.map((val, index) => (
+          <Card maxW='max'>
+            <CardBody>
+              <Stack mt='6' spacing='3'>
+                <Heading size='md'>{val?.name}</Heading>
+                <Text>{val?.url}</Text>
+                <Text color='blue.600'>Username: {val?.username}</Text>
+                <Text color='blue.600'>Password: {val?.password}</Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing='2'>
+                <Button variant='solid' colorScheme='blue'>
+                  Edit
+                </Button>
+                <Button variant='ghost' colorScheme='blue'>
+                  Delete
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+        ))}
+      </Flex>
+    </>
   );
 };
 
